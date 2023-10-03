388ada4746a03e0e1190d8feb6d7dbb467578cef
ShahriarMostafiz <shahriarmostafiz19@gmail.com>
2023-10-03 22:38:38 +0600

    first

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar/Navbar';
+import Header from '../Shared/Header/Header';
+
+const Login = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Navbar></Navbar>
+            Login Here
+        </div>
+    );
+};
+
+export default Login;
